308666607ff0ccd225317260f067f7f987ce169e
Abraham12611 <abraham.dahunsi@gmail.com>
2025-07-19 11:17:19 +0100

    stage-5

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -1,62 +1,16 @@
-const { whenProd } = require('@craco/craco');
-
 module.exports = {
-  webpack: {
-    configure: (webpackConfig, { env, paths }) => {
-      // Ignore all warnings in production
-      if (env === 'production') {
-        webpackConfig.ignoreWarnings = [/./];
-
-        // Disable source maps in production to reduce bundle size
-        webpackConfig.devtool = false;
-
-        // Ignore all ESLint warnings and errors
-        webpackConfig.stats = {
-          ...webpackConfig.stats,
-          warnings: false,
-          errors: false,
-          errorDetails: false,
-          children: false,
-          chunks: false,
-          chunkModules: false,
-          modules: false,
-          reasons: false,
-          usedExports: false,
-          providedExports: false,
-          optimizationBailout: false,
-          performance: false,
-          source: false
-        };
-      }
-
-      // Disable ESLint plugin completely
-      const eslintPlugin = webpackConfig.plugins.find(
-        plugin => plugin.constructor.name === 'ESLintWebpackPlugin'
-      );
-      if (eslintPlugin) {
-        webpackConfig.plugins = webpackConfig.plugins.filter(
-          plugin => plugin.constructor.name !== 'ESLintWebpackPlugin'
-        );
-      }
-
-      return webpackConfig;
-    },
-  },
   eslint: {
     enable: false,
   },
   typescript: {
     enableTypeChecking: false,
   },
-  devServer: {
-    overlay: false,
+  webpack: {
+    configure: (webpackConfig, { env }) => {
+      if (env === 'production') {
+        webpackConfig.devtool = false;
+      }
+      return webpackConfig;
+    },
   },
-  ...whenProd(() => ({
-    // Production-only configurations
-    performance: {
-      hints: false,
-      maxEntrypointSize: 512000,
-      maxAssetSize: 512000
-    }
-  }), {})
 };
